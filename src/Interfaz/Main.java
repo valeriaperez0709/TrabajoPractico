f0581cdb586c4bfee6d5a2066c83c58950d227ba
fedriki060 <fedbetara@gmail.com>
2026-03-10 10:43:57 -0500

    Main.java para poder ejecutar la interfaz

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -2,7 +2,7 @@ package Interfaz;
 import javafx.application.Application;
 
 public class Main {
-    public static void main(String[] args) {
+        static void main(String[] args) {
         Application.launch(InterfazPrincipal.class, args);
     }
 }
